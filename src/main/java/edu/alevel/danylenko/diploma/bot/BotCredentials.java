b0a64109d5b252778d928f71d4863599bd277bdc
Andrii <59803704+Digifan@users.noreply.github.com>
2022-11-16 17:21:58 +0200

    Update BotCredentials.java
    hide my bot token

--- src/main/java/edu/alevel/danylenko/diploma/bot/BotCredentials.java
+++ src/main/java/edu/alevel/danylenko/diploma/bot/BotCredentials.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Data
 @Configuration
 public class BotCredentials {
-    private final String token = "5763054631:AAEhBbM37hMn-rTVlH9MTfiBBlkuPAV_IWU";
+    private final String token = "YOUR_TOKEN";
     private final String username = "urvacancies_bot";
 
 
